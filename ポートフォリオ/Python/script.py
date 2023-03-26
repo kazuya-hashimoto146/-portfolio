991b135a9c3ddf5d99ced8d80462c8603ee31cf6
橋本和哉 <rin@iMac.local>
2023-03-26 18:31:12 +0900

    Python file fix

--- ポートフォリオ/Python/script.py
+++ ポートフォリオ/Python/script.py
@@ -4,13 +4,17 @@ import utils
 import random
 
 print('じゃんけんをはじめます！')
+#検証段階では文字列はクォーテーションで囲む
 player_name = input('名前を入力してください：')
+
 print('何を出しますか？（0: グー, 1: チョキ, 2: パー）')
 player_hand = int(input('数字で入力してください：'))
 
+#0〜2の正しい数値が入っているか判定
 if utils.validate(player_hand):
     computer_hand = random.randint(0, 2)
     
+    #名前が入力されているか判定し、分岐させる
     if player_name == '':
         utils.print_hand(player_hand)
     else:
