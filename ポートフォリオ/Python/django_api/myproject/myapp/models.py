b3b946f316356614da6b875e5ce3d77f2d3f2313
橋本和哉 <rin@iMac.local>
2023-04-10 02:57:04 +0900

    api admin fix

--- ポートフォリオ/Python/django_api/myproject/myapp/models.py
+++ ポートフォリオ/Python/django_api/myproject/myapp/models.py
@@ -1,6 +1,20 @@
 from django.db import models
 
 class Artist(models.Model):
-    name = models.CharField(max_length=200)
+    #アーティスト名
+    name = models.CharField(max_length=50)
+    #代表作品名
+    discography = models.CharField(max_length=200)
+    #活動開始時期
     debut_date = models.DateField()
-    agency = models.CharField(max_length=200)
+    #レーベル
+    agency = models.CharField(max_length=50)
+    #音楽ジャンル
+    GENRE_CHOICES = [
+        ('pop', 'Pop'),
+        ('rock', 'Rock'),
+        ('metal', 'Metal'),
+        ('hard rock', 'Hard Rock'),
+        ('progressive', 'Progressive'),
+    ]
+    genre = models.CharField(max_length=50, choices=GENRE_CHOICES, null=True, blank=True)
--- ポートフォリオ/Python/django_api/myproject/myproject/urls.py
+++ ポートフォリオ/Python/django_api/myproject/myproject/urls.py
@@ -1,6 +1,8 @@
+from django.contrib import admin
 from django.urls import path
 from myapp.views import ArtistListCreate
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('artists/', ArtistListCreate.as_view()),
 ]
