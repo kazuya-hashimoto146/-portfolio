e103f69a5a00c3d3e03e5e13a61cc87cc49b4777
橋本和哉 <rin@iMac.local>
2023-04-10 21:14:37 +0900

    template html fix

--- ポートフォリオ/Python/django_api/myproject/myapp/serializers.py
+++ ポートフォリオ/Python/django_api/myproject/myapp/serializers.py
@@ -4,4 +4,4 @@ from .models import Artist
 class ArtistSerializer(serializers.ModelSerializer):
     class Meta:
         model = Artist
-        fields = ['id', 'name', 'debut_date', 'agency']
+        fields = ['id', 'name', 'discography', 'debut_date', 'agency', 'genre']
